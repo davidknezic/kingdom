1ad3542fa5b60667cf9227217a99cf62d36c54bc
Sven Tschui <github@sventschui.ch>
2015-10-03 16:04:44 +0200

    Add some sidebar magic

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -3,7 +3,7 @@ import actions from '../actions'
 import stores from '../stores'
 import alt from 'alt'
 import Map from './map'
-import Side from './side'
+import Sidebar from './sidebar'
 import mui from 'material-ui'
 import {Table,TableHeader,TableBody,TableRow,TableHeaderColumn,TableRowColumn,TextField, AppBar} from 'material-ui'
 import classNames from 'classnames'
@@ -41,30 +41,10 @@ export default class Main extends React.Component {
   }
 
   render() {
-    var editSvg = '<path d="M50 5C25.1 5 5 25.1 5 50s20.1 45 45 45 45-20.1 45-45S74.9 5 50 5zM25.6 73.7l7.6-16.4 8.8 8.8-16.4 7.6zM43 65.2l-8.8-8.8 23.2-23.2 8.8 8.8L43 65.2zM67.1 41l-8.8-8.8 9.4-9.4 8.8 8.8-9.4 9.4z"/>';
-    var deleteSvg = '<path d="M50 5C25.147 5 5 25.147 5 50s20.147 45 45 45 45-20.147 45-45S74.853 5 50 5zm27.463 63.31l-9.153 9.153L50 59.153l-18.31 18.31-9.154-9.153L40.846 50l-18.31-18.31 9.156-9.154L50 40.846l18.31-18.31 9.153 9.155L59.153 50l18.31 18.31z"/>';
-
     return (
       <div className="main">
         <div className="main__sidebar" >
-          <AppBar
-            title="Hello"
-            showMenuIconButton={false} />
-
-          {['Foo','Bar','baz'].map((title, index) => {
-            var classes = classNames({ 'sidebar__item__content': true, 'sidebar__item__content--active': index == 0 });
-
-            return (
-              <div className="sidebar__item" >
-                <div className="sidebar__item__title" >
-                  <div className="sidebar__item__title__text" >{title}</div>
-                  <svg className="sidebar__item__title__action" viewBox="0 0 100 100" dangerouslySetInnerHTML={{__html: editSvg }} />
-                  <svg className="sidebar__item__title__action" viewBox="0 0 100 100" dangerouslySetInnerHTML={{__html: deleteSvg }} />
-                </div>
-                <div className={classes} >..</div>
-              </div>
-            )
-          })}
+          <Sidebar />
         </div>
         <div className="main__map">
           <Map />
