a372b5f62a2468fcd121bba41553e9a496ab8522
Pascal Reichmuth <pascal.reichmuth@gmail.com>
2015-10-03 19:17:32 +0200

    Fix mongo mongo mongo mongo config

--- api/mongo-config.js
+++ api/mongo-config.js
@@ -5,13 +5,7 @@ var url = "mongodb://localhost:27017/kingdom"
 if(process.env.VCAP_APPLICATION) {
   let appEnv = cfenv.getAppEnv();
 
-  console.log(appEnv);
-
-  console.log(appEnv.getService('kingdom_mongo'))
-
-  console.log(appEnv.getServiceUrl('kingdom_mongo'))
-
-  url = appEnv.getService('kingdom_mongo')['uri'];
+  url = appEnv.getServiceURL('kingdom_mongo')
 }
 
 export default {
