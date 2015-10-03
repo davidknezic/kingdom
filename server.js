384af1ae6e3e7e1be29a411299d777572a3d0a7d
David Knezić <davidknezic@gmail.com>
2015-10-03 12:23:15 +0200

    Make port configurable from ENV

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ import http from 'http'
 import colors from 'colors'
 import api from './api'
 
-http.createServer(api).listen(8001, '127.0.0.1', function () {
+http.createServer(api).listen(process.env.PORT || 8001, '127.0.0.1', function () {
   console.log(`API server ${'running'.green.bold} on ${'http://127.0.0.1:8001'.cyan}`)
 })
