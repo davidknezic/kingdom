c35674cdcd8e0f8f7bd973622fd95851b7bf666a
David Knezić <davidknezic@gmail.com>
2015-10-05 23:49:07 +0200

    Make mongo endpoint settable with env vars

--- api/mongo-config.js
+++ api/mongo-config.js
@@ -1,11 +1,21 @@
 import cfenv from 'cfenv'
+import colors from 'colors'
 
 var url = "mongodb://localhost:27017/kingdom"
 
-if(process.env.VCAP_APPLICATION) {
-  let appEnv = cfenv.getAppEnv();
+if (process.env.VCAP_APPLICATION) {
+  let appEnv = cfenv.getAppEnv()
 
   url = appEnv.getServiceURL('kingdom_mongo')
+}
+
+let host = process.env.MONGODB_PORT_27017_TCP_ADDR
+let port = process.env.MONGODB_PORT_27017_TCP_PORT
+
+if (host && port) {
+  url = `mongodb://${host}:${port}/kingdom`
+
+  console.log(`using mongodb hosted on '${host.green}:${port.green}'`)
 }
 
 export default {
